d30192413b3be600fe035e799abee056b91a82c2
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-29 00:36:04 -0400

    teams rendered

--- src/conponents/Pots.jsx
+++ src/conponents/Pots.jsx
@@ -4,10 +4,34 @@ import data from "../data";
 const Pots = () => {
   return (
     <div className="Container">
-      <div>
+      <div className="Pot1">
         <h2>pot</h2>
         <ul>
           {data.pot1.map((team, index) => {
+            return <li key={index}>{team}</li>;
+          })}
+        </ul>
+      </div>
+      <div className="Pot2">
+        <h2>pot2</h2>
+        <ul>
+          {data.pot2.map((team, index) => {
+            return <li key={index}>{team}</li>;
+          })}
+        </ul>
+      </div>
+      <div className="Pot1">
+        <h2>pot3</h2>
+        <ul>
+          {data.pot3.map((team, index) => {
+            return <li key={index}>{team}</li>;
+          })}
+        </ul>
+      </div>
+      <div className="Pot4">
+        <h2>pot4</h2>
+        <ul>
+          {data.pot4.map((team, index) => {
             return <li key={index}>{team}</li>;
           })}
         </ul>
