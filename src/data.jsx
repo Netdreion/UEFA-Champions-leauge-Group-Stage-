0a4f386124031915ad1e449beda9a6f0214e6d23
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-29 00:21:25 -0400

    data created

---
+++ src/data.jsx
@@ -0,0 +1,41 @@
+const data = {
+  pot1: [
+    "Manchester City",
+    "Sevilla",
+    "Barcelona",
+    "Napoli",
+    "Bayern Munich",
+    "Paris-Saint Germain",
+    "Benfica",
+    "Feyenoord",
+  ],
+  pot2: [
+    "Real Madrid",
+    "Manchester United",
+    "Inter Milan",
+    "Borussia Dortmund",
+    "Atletico Madrid",
+    "RB Leipzig",
+    "Porto",
+    "Arsenal",
+  ],
+  pot3: [
+    "Shakhtar Donetsk",
+    "Red Bull Salzburg",
+    "AC Milan",
+    "Lazio",
+    "Red Star Belgrade",
+  ],
+  pot4: [
+    "Young Boys",
+    "Galatasaray",
+    "Braga",
+    "Antwerp",
+    "Copenhagen",
+    "PSV",
+    "Union Berlin",
+    "Lens",
+  ],
+};
+
+export default data;
