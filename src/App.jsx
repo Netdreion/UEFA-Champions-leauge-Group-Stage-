0d0cec50b1f24cc0ff3add794b2c63da84ebce9f
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-30 23:16:43 -0400

    passed to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import data from "./data";
 import Pots from "./conponents/Pots";
-
+import GroupDraws from "./conponents/Groups";
 import "./App.css";
 
 function App() {
+  const draw = () => {
+    const pot1Draws = data.pot1(Math.floor(Math.random) * data.pot1.lenght);
+  };
   return (
     <>
       <Pots />
+      <GroupDraws />
     </>
   );
 }
