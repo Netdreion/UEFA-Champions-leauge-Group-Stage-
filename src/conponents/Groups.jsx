b89ad92e5bdbabcf8b353a3a332a8f15b8d35eac
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-30 23:08:41 -0400

    Group component created

--- src/conponents/Groups.jsx
+++ src/conponents/Groups.jsx
@@ -3,40 +3,56 @@ import data from "../data";
 
 const GroupDraws = () => {
   return (
-    <div className="Container">
-      <div className="Pot1">
-        <h2>pot</h2>
+    <div>
+      <div>
+        <h2>group A</h2>
         <ul>
-          {data.pot1.map((team, index) => {
-            return <li key={index}>{team}</li>;
-          })}
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
         </ul>
       </div>
-      <div className="Pot2">
-        <h2>pot2</h2>
+      <div>
+        <h2>group B</h2>
         <ul>
-          {data.pot2.map((team, index) => {
-            return <li key={index}>{team}</li>;
-          })}
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
         </ul>
       </div>
-      <div className="Pot1">
-        <h2>pot3</h2>
+      <div>
+        <h2>group C</h2>
         <ul>
-          {data.pot3.map((team, index) => {
-            return <li key={index}>{team}</li>;
-          })}
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
         </ul>
       </div>
-      <div className="Pot4">
-        <h2>pot4</h2>
+      <div>
+        <h2>group D</h2>
         <ul>
-          {data.pot4.map((team, index) => {
-            return <li key={index}>{team}</li>;
-          })}
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
         </ul>
       </div>
     </div>
   );
 };
-export default groupDraws;
+export default GroupDraws;
