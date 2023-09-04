c1e468e0ec7592f26cdef5245325aac6d8996d7e
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-09-03 21:35:52 -0400

    css added

--- src/conponents/Groups.jsx
+++ src/conponents/Groups.jsx
@@ -2,68 +2,84 @@ import React from "react";
 
 const GroupDraws = ({ groups }) => {
   return (
-    <div>
-      <div>
-        <h2>group A</h2>
-        <ul>
+    <div className="group-draws-container">
+      <div className="group">
+        <h2 className="group-title">group A</h2>
+        <ul className="team-list">
           {groups.groupA.map((team, index) => (
-            <li key={index}>{team}</li>
+            <li key={index} className="team">
+              {team}
+            </li>
           ))}
         </ul>
       </div>
-      <div>
-        <h2>group B</h2>
-        <ul>
+      <div className="group">
+        <h2 className="group-title">group B</h2>
+        <ul className="team-list">
           {groups.groupB.map((team, index) => (
-            <li key={index}>{team}</li>
+            <li key={index} className="team">
+              {team}
+            </li>
           ))}
         </ul>
       </div>
-      <div>
-        <h2>group C</h2>
-        <ul>
+      <div className="group">
+        <h2 className="group-title">group C</h2>
+        <ul className="team-list">
           {groups.groupC.map((team, index) => (
-            <li key={index}>{team}</li>
+            <li key={index} className="team">
+              {team}
+            </li>
           ))}
         </ul>
       </div>
-      <div>
-        <h2>group D</h2>
-        <ul>
+      <div className="group">
+        <h2 className="group-title">group D</h2>
+        <ul className="team-list">
           {groups.groupD.map((team, index) => (
-            <li key={index}>{team}</li>
+            <li key={index} className="team">
+              {team}
+            </li>
           ))}
         </ul>
       </div>
-      <div>
-        <h2>group E</h2>
-        <ul>
+      <div className="group">
+        <h2 className="group-title">group E</h2>
+        <ul className="team-list">
           {groups.groupE.map((team, index) => (
-            <li key={index}>{team}</li>
+            <li key={index} className="team">
+              {team}
+            </li>
           ))}
         </ul>
       </div>
-      <div>
-        <h2>group F</h2>
-        <ul>
+      <div className="group">
+        <h2 className="group-title">group F</h2>
+        <ul className="team-list">
           {groups.groupF.map((team, index) => (
-            <li key={index}>{team}</li>
+            <li key={index} className="team">
+              {team}
+            </li>
           ))}
         </ul>
       </div>
-      <div>
-        <h2>group G</h2>
-        <ul>
+      <div className="group">
+        <h2 className="group-title">group G</h2>
+        <ul className="team-list">
           {groups.groupG.map((team, index) => (
-            <li key={index}>{team}</li>
+            <li key={index} className="team">
+              {team}
+            </li>
           ))}
         </ul>
       </div>
-      <div>
-        <h2>group H</h2>
-        <ul>
+      <div className="group">
+        <h2 className="group-title">group H</h2>
+        <ul className="team-list">
           {groups.groupH.map((team, index) => (
-            <li key={index}>{team}</li>
+            <li key={index} className="team">
+              {team}
+            </li>
           ))}
         </ul>
       </div>
