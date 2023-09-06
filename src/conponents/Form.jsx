750024090efaeb887830e28e4939b208a0792946
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-09-05 21:23:11 -0400

    FormField component created

---
+++ src/conponents/Form.jsx
@@ -0,0 +1,25 @@
+
+
+const FormField=()=>{
+
+    return(
+        <div>
+            <form>
+                <div>
+                <h3>particepent</h3>
+                <input>name of the particepent</input>
+                </div>
+<div>
+            <h3> who would be the star of the tournument</h3>
+            <input placeholder="star of the tournament"></input>
+            <h3> who would be the top scorer</h3>
+            <input placeholder="Goal King"></input>
+            <h3> who would be the top assister </h3>
+            <input placeholder="assits demon"></input>
+            </div>
+           
+            </form>
+        </div>
+        export default FormField;
+    )
+}
